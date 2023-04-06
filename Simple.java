3d523bc2fc74868bacd32ffea31e014fdb8761e3
Sivsankar-Nayak <sivsankarnayak.7@gmail.com>
2023-04-06 23:23:27 +0530

    "if else logic"

--- Simple.java
+++ Simple.java
@@ -1,7 +1,14 @@
-    class Simple{  
-        public static void main(String args[]){  
-         System.out.println("Hello Java");  
-         System.out.println("Learning Java Programming"); 
+class Simple {
+    public static void main(String args[]) {
+        int x = 10;
+        int y = 7;
+        if (x + y > 20) {
+            System.out.println("x+y is greater then 20");
+        } else {
+            System.out.println("its not");
+        }
+        System.out.println("Hello Java");
+        System.out.println("Learning Java Programming");
 
-        }  
-    }  
+    }
+}
